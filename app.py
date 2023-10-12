4630db669fb275fdb801fc9a266b5c0fc8d35156
DarthVatyr <147564455+DarthVatyr@users.noreply.github.com>
2023-10-12 16:40:16 +0000

    added services page

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("home.html")
+
+@app.route("/services")
+def welcome():
+    return render_template("services.html")
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
