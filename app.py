a806ede50cb6da0c023b306dba1d80848cdc6a21
DarthVatyr <147564455+DarthVatyr@users.noreply.github.com>
2023-10-13 00:41:41 +0000

    added nav to welcome

--- app.py
+++ app.py
@@ -2,30 +2,28 @@ from flask import Flask, render_template, redirect, url_for, request, flash, ses
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def welcome():
-    return render_template("welcome.html")
+    return render_template('welcome.html')
 
 # Route for handling the login page logic
-@app.route("/login", methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for("home"))
+            return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-@app.route("/home")
+@app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
-@app.route("/trade")
+@app.route('/trade')
 def services():
-    return render_template("trade.html")
+    return render_template('trade.html')
 
-
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
